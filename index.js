ff16560e8518b1cad2ed7adeeb99644ff1c1d0fd
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-01-07 12:31:42 -0700

    commit tree b7d77905e6879a9e2192e133fd0d663f76f26da6

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+class Cat {
+    constructor(name){
+        this.name = name
+    }
+
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name){
+        this.name = name
+    }
+
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name,gender){
+        this.name = name;
+        this.gender = gender
+    }
+
+    speak(){
+        let answer;
+        if (this.gender === "male"){
+            answer = `It's me! ${this.name}, the parrot!`
+        }
+        else {
+            answer = `${this.name} says squawk!`
+        }
+
+        return answer
+    }
+}
+
